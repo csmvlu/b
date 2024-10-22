4258dcd6cbfb7ef597698caad87a9245be2303f2
csmvlu <crnitin777@gmail.com>
2024-10-22 06:28:25 +0530

    Add files via upload

---
+++ rsa.py
@@ -0,0 +1,29 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
+
+keyPair = RSA.generate(1024)
+
+pubKey = keyPair.publickey()
+print(f"Public key: (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
+
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+print("\n")
+
+# Encryption
+msg = b'MVLU College'  # Convert the message to bytes
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg)
+print("Encrypted:", binascii.hexlify(encrypted).decode('ascii'))
+print("\n")
+
+# Decryption
+decryptor = PKCS1_OAEP.new(keyPair)
+decrypted = decryptor.decrypt(encrypted)
+# Convert the decrypted bytes back to the original message
+decrypted_message = decrypted.decode('utf-8')
+print("Decrypted message:", decrypted_message)
